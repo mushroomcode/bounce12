188cfa30ef5504d4843b93ff0e82fd41465445ed
joyyue <live_orevil@126.com>
2020-02-12 22:24:00 +0800

    add the forward things

--- src/main/java/com/Controller/HunterLong2ShortController.java
+++ src/main/java/com/Controller/HunterLong2ShortController.java
@@ -8,6 +8,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 //import lombok.extern.slf4j.Slf4j;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -92,8 +94,20 @@ public class HunterLong2ShortController {
      */
     //    @ResponseBody
     @GetMapping(value = "/forward")
-    public String requestForward(HttpServletRequest request) {
+    public String requestForward() {
         return "forward:/hunter/index/forward";
+    }
+
+    /**
+     * 转发的方法2
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @GetMapping(value = "/forward2")
+    public void requestForward(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/hunter/index/forward").forward(request, response);
     }
 
 }
