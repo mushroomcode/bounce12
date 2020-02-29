f5f2dbff43eff187bfed2a14f11c15162cfa9e27
joyyue <live_orevil@126.com>
2020-02-29 14:37:02 +0800

    @Order指令的使用

--- src/main/java/com/Controller/HunterController.java
+++ src/main/java/com/Controller/HunterController.java
@@ -1,25 +1,45 @@
 package com.Controller;
 
+import com.Bean.HunterCarYellow1;
 import com.Entity.Impl.HunterImpl;
 import com.Service.HunterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.Max;
 import java.util.List;
 
 /**
  * Create by Joyyue sheting on 2018/8/25
  */
-@Profile("dev")
+
+//@Profile("dev")
 @RestController
-public class HunterController {
+@PropertySource( name="jdbc-bainuo-dev.properties",value= {"classpath:/properties/hunter.properties"},
+        ignoreResourceNotFound = false,encoding="UTF-8")
+@ConfigurationProperties(prefix = "hunter")
+public class HunterController  {
 
     @Autowired
     private HunterService hunterService;
+
+//    @ConfigurationProperties()
+    private HunterCarYellow1 hunterCarYellow1;
+
+    @Value("${driver-username}")
+    private String username1;
+
+    @Value("${driver-class-name}")
+    private String dnc;
+
+    private String counts;
+
+    private String hunterName;
+
+//    @Value("${beans}")
+//    private String beans;
 
 //    @RequestMapping(value = "/hunter", method = RequestMethod.POST)
 //    public String hunterAdd(HunterImpl hunter)  {
@@ -37,6 +57,15 @@ public class HunterController {
     @RequestMapping("/allHunters")
     public List<HunterImpl> queryAll() {
         return hunterService.queryAll();
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/showAttr")
+    public String showProperties() {
+        System.out.println(hunterName);
+        System.out.println(counts);
+        return "driver-class-name: " + dnc + " driver-username: " + username1;
     }
 
 }
